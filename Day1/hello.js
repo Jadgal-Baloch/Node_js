767552bc03c40dc026c334e7bf9732e1b91344eb
Legend9T9 <150684451+Legend9T9@users.noreply.github.com>
2024-08-08 00:21:54 +0500

    i try module  but it is showing an error

--- Day1/hello.js
+++ Day1/hello.js
@@ -3,3 +3,11 @@ console.log("Task of day one done");
 console.log("Hi github b ho gaya hai");
 console.log("GitHUB done");
 // __________________________________________
+
+// Module in node
+
+const newindex = require("./newIndex");
+console.log(newindex,'k se ho ap log');
+
+
+console.log(add(3,3));
---
+++ Day1/newIndex.js
@@ -0,0 +1,7 @@
+function add(a , b) {
+    return a + b
+}
+
+
+Module.exports = "Hello dostoon";
+

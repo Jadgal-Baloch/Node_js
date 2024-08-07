27fd72f4409fc6479cd13c6d3670ff8580470cab
Legend9T9 <150684451+Legend9T9@users.noreply.github.com>
2024-08-07 12:18:41 +0500

    i change some thing for try

--- Day1/hello.js
+++ Day1/hello.js
@@ -2,3 +2,4 @@ console.log("bhai kiya hall hai");
 console.log("Task of day one done");
 console.log("Hi github b ho gaya hai");
 console.log("GitHUB done");
+// __________________________________________

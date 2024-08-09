d773e285f5ab456edcc4d064630964ed5426ad86
Legend9T9 <150684451+Legend9T9@users.noreply.github.com>
2024-08-09 17:26:47 +0500

    i have learn module
    i have done module topic now i am going to learn fs= file systam

--- Day1/hello.js
+++ Day1/hello.js
@@ -1,13 +1,24 @@
-console.log("bhai kiya hall hai");
-console.log("Task of day one done");
-console.log("Hi github b ho gaya hai");
-console.log("GitHUB done");
+// console.log("bhai kiya hall hai");
+// console.log("Task of day one done");
+// console.log("Hi github b ho gaya hai");
+// console.log("GitHUB done");
 // __________________________________________
 
 // Module in node
 
+
+// frist way to import module 
+
 const newindex = require("./newIndex");
-console.log(newindex,'k se ho ap log');
+console.log('here is maths result', newindex.addfn(3 , 3)); 
+console.log('here is maths result', newindex.subfn(9 , 6)); 
 
 
-console.log(add(3,3));
+// another way to import module ;)
+const {addfn , subfn} = require('./newIndex');
+
+console.log('i am adding ' ,addfn(3,90),'i am subtracting' ,subfn(90,9));
+
+
+
+// console.log(add(3,3));
--- Day1/newIndex.js
+++ Day1/newIndex.js
@@ -1,7 +1,25 @@
-function add(a , b) {
+// frist point it is nacessary to expot
+
+
+
+ function add(a , b) {
     return a + b
-}
+};
+ function sub(a , b) {
+     return a - b
+ };
+
+// frist way to export it is single export
+
+module.exports = {
+addfn:add,
+ subfn:sub,
+
+ };
 
 
-Module.exports = "Hello dostoon";
+// 2nd way to export  it is multiple export
+
+exports.add=(a , b) => a + b;
+exports.sub=(a ,b) => a-b;
 
